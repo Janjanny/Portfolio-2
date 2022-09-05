e49550bd67de1d3418d9d11233c8cb8a94ab2ce9
Janjanny <janjanbeboso0912@gmail.com>
2022-09-05 22:06:08 +0800

    done homepage

---
+++ script.js
@@ -0,0 +1,53 @@
+//variables for nav-bar
+let navBar = $(".nav-bar");
+let links = navBar.find("li");
+let logo = $(".logo");
+
+//textcontent variables
+let introContent = $(".text-content");
+let textItems = introContent.find('.item');
+
+//img variables
+let imgContent = $(".img-content");
+let imgItems = imgContent.find('.img');
+
+//animation variables
+var itemDelay = 100;
+var itemDelay2 = 50;
+var j = 600;
+
+
+
+//Nav Bar Animations
+logo.delay(j).animate({
+    top: "0px",
+    opacity: 1
+});
+
+
+links.each(function() {
+    $(this).delay(j).animate({
+        top: "0px",
+        opacity: 1
+    });
+    j+= itemDelay;
+});
+
+//introContent animations
+textItems.each(function() {
+    $(this).delay(j).animate({
+        left: "0px",
+        opacity: 1
+    });
+    j+=itemDelay;
+});
+
+
+//img content animations
+imgItems.each(function () {
+    $(this).delay(j).animate({
+        marginLeft: "0px",
+        opacity: 1
+    })
+    j+=itemDelay;
+})
